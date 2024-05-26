9b80b469642026cdc074a86f3f2485415740fb2e
Matvei <khlestov.matvei@mail.ru>
2024-05-26 12:29:01 +0500

    Created base project, removed storyboard references

---
+++ ChatterBox/ChatterBox/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ChatterBox
+//
+//  Created by Matvei Khlestov on 26.05.2024.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
