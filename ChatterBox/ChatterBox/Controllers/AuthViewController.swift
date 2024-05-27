5a9a5fd8178a20af21b1f3c67603030c5259b6cb
Matvei <khlestov.matvei@mail.ru>
2024-05-27 19:00:54 +0500

    Added labels for AuthViewController

--- ChatterBox/ChatterBox/Controllers/AuthViewController.swift
+++ ChatterBox/ChatterBox/Controllers/AuthViewController.swift
@@ -7,7 +7,21 @@
 
 import UIKit
 
+// MARK: - AuthViewController
 final class AuthViewController: UIViewController {
+    
+    // MARK: - UI Elements
+    private lazy var googleLabel = UILabel(
+        text: "Get started with"
+    )
+    
+    private lazy var emailLabel = UILabel(
+        text: "Or sign up with"
+    )
+    
+    private lazy var alreadyOnboardLabel = UILabel(
+        text: "Already onboard?"
+    )
 
     private lazy var emailButton = UIButton(
         title: "Email",
@@ -30,6 +44,7 @@ final class AuthViewController: UIViewController {
         isShadow: true
     )
     
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
