9c1bb59a392ca88156bc8500c2a0305880038e2b
Matvei <khlestov.matvei@mail.ru>
2024-05-27 18:59:03 +0500

    Added extension for UILabel

---
+++ ChatterBox/ChatterBox/Extensions/UILabel+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UILabel+Extension.swift
+//  ChatterBox
+//
+//  Created by Matvei Khlestov on 27.05.2024.
+//
+
+import UIKit
+
+extension UILabel {
+    convenience init(text: String) {
+        self.init()
+        
+        self.text = text
+    }
+}
